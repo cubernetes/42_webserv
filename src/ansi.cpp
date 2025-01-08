6e0a6455152f75e7ee7abbec7dbf8ae841a2a3f0
tsi <tsi@wsl>
2025-01-08 01:28:55 +0100

    Finish ansi.[ch]pp

--- src/ansi.cpp
+++ src/ansi.cpp
@@ -1,1 +1,133 @@
+// https://no-color.org/
+// NO_COLOR=1 ./webserv -> no color output
+
+#include <string>
+#include <sstream>
+#include <cstdlib>
+
 #include "ansi.hpp"
+
+using std::string;
+
+static bool no_color() {
+	const char *no_color = std::getenv("NO_COLOR");
+	if (no_color == NULL)
+		return false;
+	else if (*no_color == '\0')
+		return false;
+	return true;
+}
+
+static string itoa(int n) {
+	std::ostringstream oss;
+	oss << n;
+	return oss.str();
+}
+
+string ansi::black(string s) {
+	if (no_color())
+		return s;
+	return ANSI_BLACK + s + ANSI_RST;
+}
+
+string ansi::red(string s) {
+	if (no_color())
+		return s;
+	return ANSI_RED + s + ANSI_RST;
+}
+
+string ansi::green(string s) {
+	if (no_color())
+		return s;
+	return ANSI_GREEN + s + ANSI_RST;
+}
+
+string ansi::yellow(string s) {
+	if (no_color())
+		return s;
+	return ANSI_YELLOW + s + ANSI_RST;
+}
+
+string ansi::blue(string s) {
+	if (no_color())
+		return s;
+	return ANSI_BLUE + s + ANSI_RST;
+}
+
+string ansi::magenta(string s) {
+	if (no_color())
+		return s;
+	return ANSI_MAGENTA + s + ANSI_RST;
+}
+
+string ansi::cyan(string s) {
+	if (no_color())
+		return s;
+	return ANSI_CYAN + s + ANSI_RST;
+}
+
+string ansi::white(string s) {
+	if (no_color())
+		return s;
+	return ANSI_WHITE + s + ANSI_RST;
+}
+
+string ansi::black_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_BLACK_BG + s + ANSI_RST;
+}
+
+string ansi::red_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_RED_BG + s + ANSI_RST;
+}
+
+string ansi::green_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_GREEN_BG + s + ANSI_RST;
+}
+
+string ansi::yellow_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_YELLOW_BG + s + ANSI_RST;
+}
+
+string ansi::blue_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_BLUE_BG + s + ANSI_RST;
+}
+
+string ansi::magenta_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_MAGENTA_BG + s + ANSI_RST;
+}
+
+string ansi::cyan_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_CYAN_BG + s + ANSI_RST;
+}
+
+string ansi::white_bg(string s) {
+	if (no_color())
+		return s;
+	return ANSI_WHITE_BG + s + ANSI_RST;
+}
+
+string ansi::rgb(string s, int r, int g, int b) {
+	if (no_color())
+		return s;
+	return ANSI_CSI ANSI_RGB ";" + itoa(r) + ";" + itoa(g) + ";" + itoa(b) + "m" + s + ANSI_RST;
+}
+
+string ansi::rgb_bg(string s, int r, int g, int b) {
+	if (no_color())
+		return s;
+	return ANSI_CSI "30" "m" ANSI_CSI ANSI_RGB_BG ";" + itoa(r) + ";" + itoa(g) + ";" + itoa(b) + "m" + s + ANSI_RST;
+}
--- src/ansi.hpp
+++ src/ansi.hpp
@@ -1,3 +1,47 @@
 #pragma once /* ansi.hpp */
 
+#include <string>
 
+using std::string;
+
+#define ANSI_CSI "\x1b\x5b"
+#define ANSI_RGB    "38;2"
+#define ANSI_RGB_BG "48;2"
+#define ANSI_RST ANSI_CSI          "m"
+#define ANSI_BLACK ANSI_CSI   "30" "m"
+#define ANSI_RED ANSI_CSI     "31" "m"
+#define ANSI_GREEN ANSI_CSI   "32" "m"
+#define ANSI_YELLOW ANSI_CSI  "33" "m"
+#define ANSI_BLUE ANSI_CSI    "34" "m"
+#define ANSI_MAGENTA ANSI_CSI "35" "m"
+#define ANSI_CYAN ANSI_CSI    "36" "m"
+#define ANSI_WHITE ANSI_CSI   "37" "m"
+#define ANSI_BLACK_BG ANSI_CSI   "30;40" "m"
+#define ANSI_RED_BG ANSI_CSI     "30;41" "m"
+#define ANSI_GREEN_BG ANSI_CSI   "30;42" "m"
+#define ANSI_YELLOW_BG ANSI_CSI  "30;43" "m"
+#define ANSI_BLUE_BG ANSI_CSI    "30;44" "m"
+#define ANSI_MAGENTA_BG ANSI_CSI "30;45" "m"
+#define ANSI_CYAN_BG ANSI_CSI    "30;46" "m"
+#define ANSI_WHITE_BG ANSI_CSI   "30;47" "m"
+
+namespace ansi {
+	string black(string s);
+	string red(string s);
+	string green(string s);
+	string yellow(string s);
+	string blue(string s);
+	string magenta(string s);
+	string cyan(string s);
+	string white(string s);
+	string black_bg(string s);
+	string red_bg(string s);
+	string green_bg(string s);
+	string yellow_bg(string s);
+	string blue_bg(string s);
+	string magenta_bg(string s);
+	string cyan_bg(string s);
+	string white_bg(string s);
+	string rgb(string s, int r, int g, int b);
+	string rgb_bg(string s, int r, int g, int b);
+}
