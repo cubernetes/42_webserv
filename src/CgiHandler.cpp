75f2ad7e1cd99a6492070d76c9a363300a734e25
tsi <tsi@wsl>
2025-01-16 22:23:25 +0100

    Cleanup CgiHandler header (with reflection)

--- src/CgiHandler.cpp
+++ src/CgiHandler.cpp
@@ -2,9 +2,8 @@
 #include <string> /* std::string */
 #include <sstream> /* std::stringstream */
 
-// #include "repr.hpp"
-#include "CgiHandler.hpp"
 #include "Logger.hpp"
+#include "CgiHandler.hpp"
 
 using std::cout;
 using std::swap;
@@ -14,25 +13,32 @@ using std::stringstream;
 
 // De- & Constructors
 CgiHandler::~CgiHandler() {
+	TRACE_DTOR;
 }
 
 CgiHandler::CgiHandler() : _extension(), _program(), _id(_idCntr++) {
+	reflect(); TRACE_DEFAULT_CTOR;
 }
 
 CgiHandler::CgiHandler(const string& extension) : _extension(extension), _program(), _id(_idCntr++) {
+	reflect(); // TRACE_DEFAULT_CTOR;
 }
 
 CgiHandler::CgiHandler(const char* extension) : _extension(extension), _program(), _id(_idCntr++) {
+	reflect(); // TRACE_DEFAULT_CTOR;
 }
 
 CgiHandler::CgiHandler(const string& extension, const string& program) : _extension(extension), _program(program), _id(_idCntr++) {
+	reflect(); // TRACE_DEFAULT_CTOR;
 }
 
 CgiHandler::CgiHandler(const CgiHandler& other) : _extension(other._extension), _program(other._program), _id(_idCntr++) {
+	reflect(); TRACE_COPY_CTOR;
 }
 
 // Copy-assignment operator (using copy-swap idiom)
 CgiHandler& CgiHandler::operator=(CgiHandler other) /* noexcept */ {
+	TRACE_COPY_ASSIGN_OP;
 	::swap(*this, other);
 	return *this;
 }
@@ -46,9 +52,11 @@ void CgiHandler::set_extension(const string& value) { _extension = value; }
 void CgiHandler::set_program(const string& value) { _program = value; }
 
 void CgiHandler::swap(CgiHandler& other) /* noexcept */ {
+	TRACE_SWAP_BEGIN;
 	::swap(_extension, other._extension);
 	::swap(_program, other._program);
 	::swap(_id, other._id);
+	TRACE_SWAP_END;
 }
 
 CgiHandler::operator string() const { return ::repr(*this); }
--- src/CgiHandler.hpp
+++ src/CgiHandler.hpp
@@ -3,23 +3,22 @@
 #include <string> /* std::string */
 #include <iostream> /* std::ostream */
 
-// #include "Reflection.hpp"
-// #include "repr.hpp"
+#include "Reflection.hpp"
 
 using std::string;
 using std::ostream;
 
-class CgiHandler /* : public Reflection */ {
+class CgiHandler : public Reflection {
 public:
-		~CgiHandler(); // destructor; consider virtual if it's a base class
-		CgiHandler(); // default constructor
+		~CgiHandler();
+		CgiHandler();
 		explicit CgiHandler(const string&); // TODO: do we need?
 		explicit CgiHandler(const char*); // TODO: do we need?
-		CgiHandler(const string& extension, const string& program); // serializing constructor
-		CgiHandler(const CgiHandler& other); // copy constructor
-		CgiHandler& operator=(CgiHandler); // copy-assignment operator
-		void swap(CgiHandler&); // copy-swap idiom
-		operator string() const; // convert object to string
+		CgiHandler(const string& extension, const string& program);
+		CgiHandler(const CgiHandler& other);
+		CgiHandler& operator=(CgiHandler);
+		void swap(CgiHandler&);
+		operator string() const;
 
 		const string& get_extension() const; // TODO: can't we just make them public?
 		const string& get_program() const; // TODO: can't we just make them public?
@@ -27,16 +26,12 @@ public:
 		void set_extension(const string&); // TODO: can't we just make them public?
 		void set_program(const string&); // TODO: can't we just make them public?
 private:
-	// REFLECT(
-	// 	"CgiHandler",
-	// 	DECL(string, _extension),
-	// 	DECL(string, _program),
-	// 	DECL(unsigned int, _id)
-	// )
-	string _extension;
-	string _program;
-	unsgined int _id;
-
+	REFLECT(
+		CgiHandler,
+		DECL(string, _extension),
+		DECL(string, _program),
+		DECL(unsigned int, _id)
+	)
 	static unsigned int _idCntr;
 };
 
