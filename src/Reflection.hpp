4e586ae673774ba91de0da575971eb9fb7240de5
tsi <tsi@wsl>
2025-01-16 22:34:35 +0100

    Cleanup Reflection header with explanations and add typedefs for the reflection API

--- src/Reflection.hpp
+++ src/Reflection.hpp
@@ -4,59 +4,58 @@
 #include <utility>
 #include <map>
 
-#include "macro_magic.h"
-
 using std::string;
 using std::map;
 using std::pair;
 
-enum e_type {
-	INT,
-	INTP,
-	UINT,
-	UINTP,
-	CHAR,
-	CHARP,
-	UCHAR,
-	UCHARP,
-	SHORT,
-	SHORTP,
-	LONG,
-	LONGP,
-	ULONG,
-	ULONGP,
-	LLONG,
-	LLONGP,
-	ULLONG,
-	ULLONGP,
-	STRING,
-	STRINGP,
-	BOOL,
-	BOOLP,
-	LOGGER,
-	LOGGERP,
-	HTTPSERVER,
-	HTTPSERVERP,
-	UNKNOWN
-};
-typedef enum e_type t_type;
+class Reflection;
+
+typedef string (Reflection::*t_repr_closure)(bool json) const;
+typedef pair<t_repr_closure, const void*> t_member;
+typedef map<const char*, t_member> t_members;
 
 class Reflection {
 public:
+	// due to -Weffc++
 	virtual ~Reflection() {}
+	Reflection();
+	Reflection(const Reflection& other);
+	Reflection& operator=(Reflection other);
+	void swap(Reflection& other);
+
+	// set by the REFLECT macro
+	const char *_class;
+
+	// populated by the REFELECT macro
+	t_members _members;
+
+	// generate a string representation of the class from _members by iterating over
+	// it and using _memberToString to serialize it member by member
+	string repr(bool json = false) const;
 protected:
-	string _memberToStr(const pair<t_type, const void*>& member, bool json = false) const;
-	void reflect_member(t_type type, const char *memberId, const void *memberPtr);
-	virtual void reflect() = 0;
+	// is implemented automatically in the derived class by the REFLECT macro from macro_magic.h
+	void reflect() {} // empty by default, in case you're inheritin from this class and do the reflection in another (often post-periori, i.e. for another class that does not have reflection) way
+
+	// Each DECL macro from macro_magic.h will generate 2 functions with deterministic names.
+	// One of them is a closure that calls ::repr() for the specific member.
+	// The other one is also a closure that calls reflect_member() with the required parameters.
+	// The automatically implemented reflect() function (which is also a closure, in a way) in the derived class will call those closures
+	// which call reflect_member(), which in turn adds the repr closures to the internal map of member.
+	void reflect_member(t_repr_closure repr_closure, const char *memberId, const void *memberPtr);
+
+	// return the string representation of a member
+	string _memberToStr(const t_member& member, bool json = false) const;
+
+	// Convenience print functions
 	void print_copy_assign_op(const string& repr_other) const;
 	void print_copy_ctor(const string& repr_other, const string& repr_this) const;
 	void print_default_ctor(const string& repr_this) const;
 	void print_dtor(const string& repr_this) const;
 	void print_swap_begin(const string& repr_this, const string& repr_other) const;
 	void print_swap_end() const;
-	static string jsonEscape(string s);
-public:
-	const char *_class;
-	map<const char*, pair<t_type, const void*> > _members;
-	string repr(bool json = false) const;
+	string repr_struct(string name, t_members members, bool json = false) const;
 };
+void swap(Reflection& a, Reflection& b) /* noexcept */;
+
+#include "macro_magic.h"
+#include "repr.hpp"
