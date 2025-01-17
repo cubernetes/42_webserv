85540ebe395edfd00eafcc6835a6d03e65caccc3
tsi <tsi@wsl>
2025-01-17 03:10:37 +0100

    Change back loglevel to info

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -7,7 +7,7 @@ using std::string;
 
 namespace Constants {
 	const string defaultConfPath = "default.conf";
-	const int logLevel = Logger::logLevelTrace; // doesn't NEED to be const tho
+	const int logLevel = Logger::logLevelInfo; // doesn't NEED to be const tho
 	const bool kwargLogs = true; // doesn't NEED to be const tho
 	const bool verboseLogs = false; // doesn't NEED to be const tho
 	const bool jsonTrace = false; // doesn't NEED to be const tho
