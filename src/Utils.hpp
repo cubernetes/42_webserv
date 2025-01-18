360f8e24a54ab0ae8c9b43053915bb060ff75c59
tsi <timo42@proton.me>
2025-01-18 23:14:19 +0100

    Add comment about magic VEC macro

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -20,5 +20,7 @@ namespace Utils {
 
 #define TMP_VEC_PUSH_BACK(type, x) \
 	Utils::getTmpVec<type>().push_back(x),
+
+// Helper macro to create inplace vectors (i.e. passing as an argument), very useful sometimes
 #define VEC(type, ...) \
 	(Utils::getTmpVec<type>().clear(), FOR_EACH_ONE(TMP_VEC_PUSH_BACK, type, __VA_ARGS__) Utils::getTmpVec<type>())
