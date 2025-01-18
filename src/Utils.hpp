cbbef44039c25435e51511d85d11d94b08a7755f
tsi <timo42@proton.me>
2025-01-18 21:12:23 +0100

    Add VEC macro to create and initialize an inplace vector of any type

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -1,9 +1,24 @@
 #pragma once /* Utils.hpp */
 
+#include <vector>
 #include <string>
 
+#include "MacroMagic.h"
+
+using std::vector;
 using std::string;
 
 namespace Utils {
-	string parseArgs(int, char**);
+	string parseArgs(int ac, char **av);
+
+	template<typename T>
+	vector<T>& getTmpVec() {
+		static vector<T> _;
+		return _;
+	}
 }
+
+#define TMP_VEC_PUSH_BACK(type, x) \
+	Utils::getTmpVec<type>().push_back(x),
+#define VEC(type, ...) \
+	(Utils::getTmpVec<type>().clear(), FOR_EACH_ONE(TMP_VEC_PUSH_BACK, type, __VA_ARGS__) Utils::getTmpVec<type>())
